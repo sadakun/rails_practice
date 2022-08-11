a7ede2da6e4c9895b687b957d24e9575ffa4c0a9
sadakun <sm123kuncoro@gmail.com>
2022-08-12 05:12:30 +0700

    create, show edit and update book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,34 @@ class BooksController < ApplicationController
   end
 
   def show
-  @book = Book.find_by(id: params[:id])
+    @book = Book.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @book = Book.new(book_title: params[:book_title], book_author: params[:book_author], book_published: params[:book_published], book_description: params[:book_description])
+    @book.save
+    flash[:addMessage] = 'Book succesfuly added'
+
+    redirect_to('/index')
+  end
+
+  def edit    
+    @book = Book.find_by(id: params[:id])
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+    @book.book_title = params[:book_title]
+    @book.book_author = params[:book_author]
+    @book.book_published = params[:book_published]
+    @book.book_description = params[:book_description]
+    @book.save
+
+    flash[:updateMessage] = 'Book succesfuly updated'
+    redirect_to('/index')
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#home'
   get 'index', to: 'books#index'
   get '/show/:id' => 'books#show'
+
+  get 'new', to: 'books#new'
+  post 'new/book' => 'books#create'
+
+  get '/book/edit/:id' => 'books#edit'
+  post 'book/update/:id' => 'books#update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
